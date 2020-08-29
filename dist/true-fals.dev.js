f80117c174899c9348d8f10a21b4aa27bcd01010
tusinahmed <tusinahmed@gmail.com>
2020-08-29 07:34:48 +0600

    true vs false

---
+++ dist/true-fals.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var name = "soloiman";
+
+if (name.length > 100) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
---
+++ true-fals.js
@@ -0,0 +1,11 @@
+let name = "soloiman";
+
+
+
+if( name.length > 100){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false")
+}
