1df40b206591afde510acf47559f7ce00869442d
tusinahmed <tusinahmed@gmail.com>
2020-08-29 07:45:47 +0600

    undefine

---
+++ dist/null-undefined.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//let  pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// function add (num1, num2){
+//     console.log(num2);
+// }
+// let result = add(13);
+// console.log(num1, num2);
+// let premik = {name: "smart dude", phone:12344};
+// console.log(premik.gf);
+var fun = undefined;
+console.log(fun);
---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+//let  pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add (num1, num2){
+//     console.log(num2);
+// }
+
+// let result = add(13);
+// console.log(num1, num2);
+
+
+// let premik = {name: "smart dude", phone:12344};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
